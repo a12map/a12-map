23ca80ea26203b35ae0b374b94dcee38d97f5740
vire <code.vire@gmail.com>
2016-09-30 20:56:42 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>A12 Map</h1>
+        </div>
+        <p className="App-intro">
+          <div>
+
+          </div>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
