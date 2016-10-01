b298efd2ab64b87e862dace072aee35f84db1cd8
vire <code.vire@gmail.com>
2016-10-01 13:07:48 +0200

    removed heatmap

--- src/SimpleMap.js
+++ src/SimpleMap.js
@@ -1,79 +1,65 @@
-import React from "react";
+import React, { Component } from "react";
 import {
   GoogleMapLoader,
   GoogleMap,
 } from "react-google-maps";
 
-const generateData = (dataPointsCount) => {
-  let result = [];
+const getData = () => {
+  return fetch('http://localhost:3333/stops-test.json')
+    .then(response => response.json())
+};
 
-  for (let i = 0; i < dataPointsCount; i++) {
-    const lat = 50 + Math.random();
-    const lng = 14 + Math.random();
-    result[i] = {
-      lat, lng, count: Math.random() * 10
-    }
+export default class SimpleMap extends Component {
+
+  gmap = null;
+
+  componentDidMount() {
+    getData()
+      .then(dataPoints => {
+        dataPoints.forEach(point => {
+          new window.google.maps.Circle({
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35,
+            map: this.gmap,
+            center: {
+              lat: point.lat,
+              lng: point.lng,
+            },
+            radius: 10,
+          });
+        })
+      })
   }
-  return result
-};
 
-const updateHeatMap = (map) => {
-  const heatmap = new window.HeatmapOverlay(map,
-    {
-      // radius should be small ONLY if scaleRadius is true (or small radius is intended)
-      "radius": 30,
-      "maxOpacity": 1,
-      // scales the radius based on map zoom
-      "scaleRadius": false,
-      // if set to false the heatmap uses the global maximum for colorization
-      // if activated: uses the data maximum within the current map boundaries
-      //   (there will always be a red spot with useLocalExtremas true)
-      "useLocalExtrema": false,
-      // which field name in your data represents the latitude - default "lat"
-      latField: 'lat',
-      // which field name in your data represents the longitude - default "lng"
-      lngField: 'lng',
-      // which field name in your data represents the data value - default "value"
-      valueField: 'count'
-    }
-  );
-
-  var testData = {
-    max: 10,
-    data: generateData(100)
-  };
-
-  console.log('testData', testData)
-
-  heatmap.setData(testData);
-};
-
-const SimpleMap = props => {
-  const containerElement =(
-    <div
-      {...props.containerElementProps}
-      style={{
-        height: `100%`,
-      }}
-    />
-  );
-
-  const googleMapElement = (
-    <GoogleMap
-      ref={(map) => map ? updateHeatMap(map.props.map) : undefined}
-      defaultZoom={12}
-      defaultCenter={{ lat: 50.070569, lng: 14.419172 }}
-    />
-  );
-
-  return (
-    <section style={{ height: `90%` }}>
-      <GoogleMapLoader
-        containerElement={containerElement}
-        googleMapElement={googleMapElement}
+  render() {
+    const props = this.props;
+    const containerElement =(
+      <div
+        {...props.containerElementProps}
+        style={{
+          height: `100%`,
+        }}
       />
-    </section>
-  )
-};
+    );
 
-export default SimpleMap;
+    const googleMapElement = (
+      <GoogleMap
+        ref={(mapWrapper) => this.gmap = mapWrapper.props.map}
+        defaultZoom={12}
+        defaultCenter={{ lat: 50.070569, lng: 14.419172 }}
+      />
+    );
+
+    return (
+      <section style={{ height: `90%` }}>
+        <GoogleMapLoader
+          containerElement={containerElement}
+          googleMapElement={googleMapElement}
+        />
+      </section>
+    )
+  }
+}
