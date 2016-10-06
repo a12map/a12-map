d9c037e1d5c13b96ae6dde855ecee1714fe60f40
Eldar Gabidullin <indiegate@gmail.com>
2016-10-06 22:09:58 +0200

    Optimize dragging

--- src/util/compute-voronoi.js
+++ src/util/compute-voronoi.js
@@ -9,17 +9,28 @@ const draw = function (data, handleHover) {
   const mapOverlay = d3.select(this.getPanes().overlayMouseTarget);
   mapOverlay.html(''); // TODO this is a dirty solution how to empty
 
+  const bounds = this.map.getBounds();
+
+  const topLeftX = this.getProjection().fromLatLngToDivPixel(bounds.getSouthWest()).x;
+  const topLeftY = this.getProjection().fromLatLngToDivPixel(bounds.getNorthEast()).y;
+
   const svg = mapOverlay
     .append('div')
     .attr('class', 'SvgOverlay') // parent elem of SVG
-    .append('svg');
-
+    .append('svg')
+    .style("width", window.innerWidth + 'px')
+    .style("height", window.innerHeight + 'px')
+    .style("margin-left", topLeftX + "px")
+    .style("margin-top", topLeftY + "px")
+    ;
+  
+  const g = svg.append('g')
+    .attr('transform', `translate(${-topLeftX},${-topLeftY})`);
+  
   const googleMapProjection = (lat, lng) => {
     const pixelCoordinates = this.getProjection().fromLatLngToDivPixel(new google.maps.LatLng(lat, lng));
-    return [pixelCoordinates.x + 1000, pixelCoordinates.y + 1000];
+    return [pixelCoordinates.x, pixelCoordinates.y];
   };
-
-  const bounds = this.map.getBounds();
 
   const positions = data
     .filter(d => bounds.contains(new google.maps.LatLng(d.lat, d.lng)))
@@ -45,29 +56,29 @@ const draw = function (data, handleHover) {
     opacity: 0.4
   };
 
-  svg.selectAll('path')
+  const pointGroup = g.attr('class', 'points')
+    .selectAll('g')
     .data(v2.polygons(positions))
-    .attr(pathAttr)
     .enter()
-    .append('svg:path')
-    .attr('class', 'cell')
+    .append('g')
+    .attr('class', 'point')
+
+  pointGroup.append('svg:path')
     .attr(pathAttr)
+    .attr('class', 'cell')
     .on('mouseover', ({ data }) => {
       handleHover(data.name, data.travelTime)
     });
 
   const circleAttr = {
-    cx(d, i) { return positions[i].latLng[0]; },
-    cy(d, i) { return positions[i].latLng[1]; },
+    // cx(d, i) { return positions[i].latLng[0]; },
+    // cy(d, i) { return positions[i].latLng[1]; },
     r: 1,
     fill:'black'
   };
 
-  svg.selectAll('circle')
-    .data(positions)
-    .attr(circleAttr)
-    .enter()
-    .append('svg:circle')
+  pointGroup.append('svg:circle')
+    .attr("transform", function(d, i) { return "translate(" + positions[i].latLng[0]+ "," + positions[i].latLng[1] + ")"; })
     .attr(circleAttr)
 };
 
@@ -77,8 +88,8 @@ export function computeVoronoi(data, map, handleHover) {
   overlay.draw = draw.bind(overlay, data, handleHover);
   overlay.setMap(map);
 
-  google.maps.event.clearListeners(map, 'bounds_changed');
-  map.addListener('bounds_changed', () => {
+  google.maps.event.clearListeners(map, 'dragend');
+  map.addListener('dragend', () => {
     throttle(overlay.draw, 1000)(data, map, handleHover)
   });
 }
