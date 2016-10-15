427108a2dd782edd7bb8f37f4cc06194227342b2
vire <code.vire@gmail.com>
2016-10-15 18:27:18 +0200

    styled map

--- src/components/PurchasesGoogleMaps.js
+++ src/components/PurchasesGoogleMaps.js
@@ -5,15 +5,23 @@ import {
   withGoogleMap } from 'react-google-maps';
 
 import getColor from '../util/colors';
+import { customizeMap } from '../util/customize-map';
 
+const MAP_ACCESSOR= '__SECRET_MAP_DO_NOT_USE_OR_YOU_WILL_BE_FIRED';
 
 class PurchasesGoogleMaps extends Component {
+
+  setupMap({ context }) {
+    const map = context[MAP_ACCESSOR];
+    customizeMap(map);
+  }
+
   render() {
     const { props } = this;
     return (
       <GoogleMap
-        ref={props.onMapLoad}
-        defaultZoom={8}
+        ref={this.setupMap}
+        defaultZoom={7}
         defaultCenter={{ lat: 49.561150, lng: 16.958839 }}
         onClick={props.onMapClick}
       >
